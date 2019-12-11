b3715bef781c2c9b5d9317c784ca0d067737c463
Nick Omleianenko <omelianik@gmail.com>
2019-12-11 15:59:36 -0800

    Changed the Sign up detailes

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,6 +1,12 @@
 class BookingController < ApplicationController
     before_action :authenticate_user!
-  def booking
+    before_action :configure_permitted_parameters, if :devise_controller?
 
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :phone])
+    end
+  def booking
   end
 end
--- db/migrate/20191206232511_devise_create_users.rb
+++ db/migrate/20191206232511_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :phone,              null: false, default: ""
+
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,6 +43,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :first_name,                unique: true
+    add_index :users, :last_name,                unique: true
+    add_index :users, :phone,                unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
